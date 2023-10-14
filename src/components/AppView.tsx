5a754848afe62ff00bf2f751b6b5360d2c5aad87
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-10-14 16:17:36 +0300

    -v1

--- src/components/AppView.tsx
+++ src/components/AppView.tsx
@@ -6,9 +6,9 @@ export function AppView() {
       <div className='sir'>
         <div className='sir-body'>
           <div 
-            class='flex flex-col 2x:w-auto md:w-full sm:w-full h-auto 2xl:p-20 md:p-0 
-                   rounded-[40px] border border-slate-800 items-center
-                   justify-center drop-shadow-lg backdrop-blur-md'
+            class='flex flex-col 2x:w-auto xl:w-auto lg:w-auto md:w-full xs:w-full h-auto 2xl:p-20 md:p-0 
+                   rounded-[40px] items-center
+                   justify-center drop-shadow-lg backdrop-blur-md mt-10'
           > 
             <div class=''> <Body /> </div>
           </div>
