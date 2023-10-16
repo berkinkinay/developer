c0eb5cae52614a151e7af36884cc7ca69f1d0c5e
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-10-16 19:20:42 +0300

    -v1

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -10,7 +10,7 @@ import LastText from './LastText';
 const Body = () => {
   return (
     <div 
-      class='flex flex-col items-center justify-center 2xl:w-auto xl:w-auto lg:w-auto md:w-auto sm:w-auto xs:w-fit h-full rounded-xl gap-10 p-10 lg:p-10 sm:m-0 sm:p-0 xs:p-0'
+      class='flex flex-col 2xl:w-auto xl:w-auto lg:w-auto md:w-auto sm:w-auto xs:w-fit h-full rounded-xl gap-10 p-10 lg:p-10 sm:m-0 sm:p-0 xs:p-0'
     >
         <Header />
         <Developer />
