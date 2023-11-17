2b25f64743388d909564c4ddad3c0a87b5534176
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-11-17 16:25:19 +0300

    -v1

--- src/components/AppView.tsx
+++ src/components/AppView.tsx
@@ -1,19 +1,22 @@
 import Body from './Body'
 
-export function AppView() {
+const AppView = () => {
   return (
     <div className='title'>
-      <div className='sir'>
-        <div className='sir-body'>
-          <div 
-            class='flex flex-col 2x:w-auto xl:w-auto lg:w-auto md:w-full xs:w-full h-auto 2xl:p-20 md:p-0 
-                   rounded-[40px] items-center
-                   justify-center drop-shadow-lg backdrop-blur-md mt-10'
-          > 
-            <div class=''> <Body /> </div>
-          </div>
+      <div 
+        className='flex 2xl:pl-64 2xl:pr-64 pt-10 lg:p-0 flex-col xs:w-full p-0
+                   bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-fuchsia-600 
+                  via-gray-800 to-violet-100 2xl:h-auto lg:h-auto w-full items-center justify-center'
+      >
+        <div 
+          className='flex flex-col 2xl:h-full md:h-auto xs:w-full 2xl:w-[1350px] lg:w-[1100px] md:w-full sm:w-auto 2xl:m-20 
+                     lg:m-5 rounded-[40px] items-center bg-black'
+        >
+         
+            <Body />
         </div>
       </div>
     </div>
   )
 }
+export default AppView;
